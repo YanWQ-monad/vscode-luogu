b054717d1cf33a46849f473298bab9ee3d781781
YanWQ-monad <YanWQmonad@gmail.com>
2020-02-02 18:22:11 +0800

    feat(api): add cookie storage support

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,15 +1,22 @@
 import _ from 'axios'
+import { Cookie, CookieJar } from 'tough-cookie'
+import axiosCookieJarSupport from 'axios-cookiejar-support'
 
 export namespace API {
   export const baseURL = 'https://www.luogu.com.cn'
   export const apiURL = '/api'
   export const SEARCH_PROBLEM = (pid: string) => API.apiURL + '/problem/detail' + `/${pid}`
   export const ACCESS_TOKEN = '/OAuth2/accessToken'
+  export const cookieDomain = 'luogu.com.cn'
 }
 
-export const axios = _.create({
-  baseURL: API.baseURL
-})
+export const jar = new CookieJar();
+
+export const axios = axiosCookieJarSupport(_.create({
+  baseURL: API.baseURL,
+  withCredentials: true,
+  jar
+}))
 
 export const searchProblem = async (pid: string) =>
   axios.get(API.SEARCH_PROBLEM(pid))
